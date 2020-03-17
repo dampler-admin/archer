0118abbf25e4bd002be797fded89a5a0ee512251
dampler-admin <52274291+dampler-admin@users.noreply.github.com>
2020-03-17 11:46:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import os
 from random import random
 N = 999999
+X = 999999
 arr = [0]*N
+arr2 = []*X
 even = []
 for i in range(N):
     arr[i] = int(random() * 10) + 10
+    if arr[i] % 2 == 0:
+        even.append(i)
+for i in range(N):
+    arr2[i] = int(random() * 10) + 10
     if arr[i] % 2 == 0:
         even.append(i)
 print(arr)
